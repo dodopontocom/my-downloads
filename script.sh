464bb2c107c2680dc00339f34abacc53b35d67e5
rodolfotiago <rodolfotiago@gmail.com>
2020-04-09 14:57:31 -0300

    testing

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #help on backuping my Downloads folder
 
+download_folder="/home/${USER}/Downloads"
+bkp_folder="/tmp/download-bkp"
+
+types=(doc docx pdf jpg png mp3 mp4 avi mkv deb iso gz zip str epub)
+
+
